7a090a25fdb297523694e5fbd12591d60030cc0e
Remco van 't Veer <remco@remworks.net>
2020-03-01 16:21:26 +0100

    Upgrade deps

--- project.clj
+++ project.clj
@@ -1,11 +1,16 @@
 (defproject dragon_curve "1.0.0-SNAPSHOT"
   :description "Draw a dragon curve"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]
-                 [reagent "0.4.2"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
+                 [reagent "0.8.1"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.17"]]
+
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.18"]
+                                  [cider/piggieback "0.4.0"]]
+                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
 
   :cljsbuild {:builds {:app {:source-paths ["src"]
                              :compiler {:preamble ["reagent/react.js"]
